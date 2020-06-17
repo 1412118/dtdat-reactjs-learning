ade484578bce979e6c027629ae603866dfcf2929
141218 <datduong@DESKTOP-LHE2MMH.localdomain>
2020-06-17 11:08:20 +0700

    Render a list have similar components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoItem from './components/TodoItem'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TodoItem content="Have breakfast"/>
-        <TodoItem content="Go to work"/>
-        <TodoItem content="Go to gym"/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.todoItems = [
+      'Have breakfast',
+      'Go to work',
+      'Go to the gym'
+    ];
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {
+            this.todoItems.map((item, index) => <TodoItem key={index} content={item} />)
+          }
+          {/* <TodoItem content="Have breakfast"/>
+          <TodoItem content="Go to work"/>
+          <TodoItem content="Go to gym"/> */}
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
